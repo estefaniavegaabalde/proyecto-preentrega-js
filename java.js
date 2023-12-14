c339b94e797d342513994a7f9904e7080492dfeb
estefaniavegaabalde <vegabaldeestefania@gmail.com>
2023-12-14 19:28:48 -0300

    Segundo commit

--- java.js
+++ java.js
@@ -1,55 +1,74 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const exchangeRates = {
+    'USD': 359.46,
+    'EUR': 395.27
+    };
+
+const supportedCurrencies = Object.keys(exchangeRates);
+let conversionHistory = [];
 
 function convertCurrency(amount, currency) {
-    switch (currency) {
-        case 'USD':
-            return amount * 359,46; // Tasa de conversión ejemplo
-        case 'EUR':
-            return amount * 395,27;
-        // Añadir más casos según sea necesario
-        default:
-            return null; // En caso de moneda no soportada
+    console.log(`Intentando convertir ${amount} a ${currency}`);
+    if (currency in exchangeRates) {
+        const result = amount * exchangeRates[currency];
+        console.log(`Conversión exitosa: ${amount} a ${currency} = ${result}`);
+        return result;
+    } else {
+        console.log(`Moneda no soportada: ${currency}`);
+        return null; // En caso de moneda no soportada
     }
 }
 
-function askQuestion(query) {
-    return new Promise((resolve) => {
-        readline.question(query, resolve);
+function isCurrencySupported(currency) {
+    const isSupported = supportedCurrencies.includes(currency);
+    console.log(`¿Moneda soportada? ${currency}: ${isSupported}`);
+    return isSupported;
+}
+
+function addConversionToHistory(amount, currency, result) {
+    conversionHistory.push({ amount, currency, result });
+    console.log(`Historial actualizado: ${amount} ${currency} = ${result}`);
+}
+
+function displayConversionHistory() {
+    console.log('Mostrando historial de conversiones:');
+    conversionHistory.forEach((conversion, index) => {
+        console.log(`${index + 1}: ${conversion.amount} convertido a ${conversion.currency} = ${conversion.result}`);
     });
 }
 
-async function startConversion() {
+function startConversion() {
     console.log('Bienvenido al Conversor de Moneda');
 
     let repeat = true;
-
     while (repeat) {
-        const amount = await askQuestion('Ingrese la cantidad a convertir: ');
+        const amount = prompt('Ingrese la cantidad a convertir con moneda inicial pesos: ');
 
         if (isNaN(amount) || amount <= 0) {
             console.log('Por favor, ingrese un número válido.');
-        } else {
-            const currency = await askQuestion('Ingrese la moneda (USD, EUR, etc.): ');
-            const result = convertCurrency(parseFloat(amount), currency);
-
-            if (result === null) {
-                console.log('Moneda no soportada. Intente con otra moneda.');
-            } else {
-                console.log(`El resultado de la conversión es: ${result}`);
-            }
+            continue;
         }
 
-        const answer = await askQuestion('¿Desea realizar otra conversión? (si/no): ');
-        if (answer.toLowerCase() !== 'si') {
-            repeat = false;
+        const currency = prompt('Ingrese la moneda (USD, EUR, etc.): ');
+        if (!isCurrencySupported(currency)) {
+            console.log('Moneda no soportada. Intente con otra moneda.');
+            continue;
+        }
+
+        const result = convertCurrency(parseFloat(amount), currency);
+        if (result !== null) {
+            console.log(`El resultado de la conversión es: ${result}`);
+            addConversionToHistory(amount, currency, result);
+        } else {
+            console.log('Moneda no soportada. Intente con otra moneda.');
+        }
+
+        repeat = confirm('¿Desea realizar otra conversión?');
+        if (!repeat) {
+            displayConversionHistory();
             console.log('Gracias por usar el Conversor de Moneda. ¡Hasta luego!');
         }
     }
-
-    readline.close();
 }
 
 startConversion();
+
